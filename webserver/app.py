0654e94f625b99542ee68fda70a3c3b133e2ea55
jan-klos <janklos@protonmail.com>
2018-04-09 22:04:26 +0000

    test interface

---
+++ webserver/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+path = "../files/"
+
+@app.route("/")
+def main():
+    files = os.listdir(path)
+    template_data = { 'files' : files }
+    return render_template("main.html", **template_data)
+    
+@app.route("/", methods = ['POST'])
+def create_file():
+	file_name =  path + request.form['file_name']
+	open(file_name, 'w+')
+	return main()
+    
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 80, debug = True)
