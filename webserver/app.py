066797ce4532eaee76d30afcf0d30a4507e08dc3
jan-klos <janklos@protonmail.com>
2018-04-10 10:12:36 +0000

    file printing

--- webserver/app.py
+++ webserver/app.py
@@ -13,8 +13,17 @@ def main():
 @app.route("/", methods = ['POST'])
 def create_file():
 	file_name =  path + request.form['file_name']
-	open(file_name, 'w+')
+	file_content = request.form['file_content']
+	file = open(file_name, 'w+')
+	file.write(file_content)	
 	return main()
+
+@app.route("/<file_name>", methods = ['GET'])
+def print_file(file_name):
+    with open(path + file_name) as file:
+        file_content = file.read().replace("\n", "<br>")
+    template_data = { 'file_name' : file_name, 'file_content' : file_content }
+    return render_template("file.html", **template_data)
     
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 80, debug = True)
