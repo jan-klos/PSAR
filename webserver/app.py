954a492275d138a7854b72324ddf74db37ecfccc
jan-klos <janklos@protonmail.com>
2018-04-10 19:46:54 +0200

    fixes

--- webserver/app.py
+++ webserver/app.py
@@ -10,22 +10,22 @@ if not os.path.exists(path):
 @app.route("/")
 def main():
     files = os.listdir(path)
-    template_data = { 'files' : files }
+    template_data = { "files" : files }
     return render_template("main.html", **template_data)
     
-@app.route("/", methods = ['POST'])
+@app.route("/", methods = ["POST"])
 def create_file():
-	file_name =  path + request.form['file_name']
-	file_content = request.form['file_content']
-	file = open(file_name, 'w+')
+	file_name =  path + request.form["file_name"]
+	file_content = request.form["file_content"]
+	file = open(file_name, "w+")
 	file.write(file_content)	
 	return main()
 
-@app.route("/<file_name>", methods = ['GET'])
+@app.route("/<file_name>", methods = ["GET"])
 def print_file(file_name):
     with open(path + file_name) as file:
         file_content = file.read().replace("\n", "<br>")
-    template_data = { 'file_name' : file_name, 'file_content' : file_content }
+    template_data = { "file_name" : file_name, "file_content" : file_content }
     return render_template("file.html", **template_data)
     
 if __name__ == "__main__":
