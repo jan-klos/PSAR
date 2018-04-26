9bcfe0e2d3dfc7fcdeef41d26f0aff5c91c194f1
jan-klos <janklos@protonmail.com>
2018-04-26 20:03:34 +0200

    error messages

--- webserver/app.py
+++ webserver/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request, redirect, flash
-import os, util, settings
+from flask import Flask, render_template, request, redirect
+import socket  # for IP address testing
+import os
+import util
+import settings
 
 app = Flask(__name__)
 
@@ -7,41 +10,55 @@ if not os.path.exists(settings.FILES_PATH):
     os.makedirs(settings.FILES_PATH)
 
 # folder for temporary image files storing
-util.create_static("/static/img")
+util.create_tmp()
+
 
 @app.route("/")
-def main():
-    util.clear_static() # removes temporary image files
+def main(error=""):
+    util.clear_tmp()  # removes temporary image files
     file_list = os.listdir(settings.FILES_PATH)
     file_dict_list = util.get_file_dict_list(file_list)
-    template_data = { "files" : file_dict_list }
-    return render_template("main.html", **template_data)
-    
-@app.route("/", methods = ["POST"])
-def add_file():
-    if "create_file_submit" in request.form and request.form["file_name"] != "":
-        util.create_file(request.form["file_name"], request.form["file_content"])
-    elif "upload_file_submit" in request.form and request.files["file"].filename != "":
-        util.upload_file(request.files["file"])
-    return main()
+    template_data = {"files": file_dict_list}
+    return render_template("main.html", error=error, **template_data)
 
-@app.route("/files/<file_name>", methods = ["POST"])
+
+@app.route("/", methods=["POST"])
+def add_file():
+    error = ""
+    if "create_file_submit" in request.form:
+        if request.form["file_name"] == "":
+            error = "You must name the file"
+        else:
+            util.create_file(request.form["file_name"], request.form["file_content"])
+    elif "upload_file_submit" in request.form:
+        if not request.files.get('file', None):
+            error = "You must choose a file"
+        else:
+            util.upload_file(request.files["file"])
+    return main(error=error)
+
+
+@app.route("/files/<file_name>", methods=["POST"])
 def send_file(file_name):
     send_address = request.form["send_address"]
-    #proxy
+    try:
+        socket.inet_pton(socket.AF_INET, send_address)
+    except socket.error:
+        return show_file(file_name, error="Incorrect IP address")
     print "send_file(): \n    file_name: {0} \n    address: {1}".format(file_name, send_address)
-    #proxy
-    return redirect("/", code = 302)
+    return redirect("/", code=302)
 
-@app.route("/files/<file_name>", methods = ["GET"])
-def show_file(file_name):
+
+@app.route("/files/<file_name>", methods=["GET"])
+def show_file(file_name, error=""):
     file_dict = util.get_file_dict(file_name)
     if file_dict["type"] in ("img", "pdf"):
-        util.copy_file_to_static(file_name)
-        template_data = { "file" : file_dict}
+        util.copy_file_to_tmp(file_name)
+        template_data = {"file": file_dict}
     else:
-        template_data = { "file" : file_dict, "file_content" : util.get_file_content(file_name) }
-    return render_template("file.html", **template_data)
+        template_data = {"file": file_dict, "file_content": util.get_file_content(file_name)}
+    return render_template("file.html", error=error, **template_data)
+
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 80, debug = True)
+    app.run(host="0.0.0.0", port=80, debug=True)
--- webserver/util.py
+++ webserver/util.py
@@ -1,8 +1,10 @@
 import os, shutil, settings
 from werkzeug.utils import secure_filename
 
+
 def get_file_size(file):
     return os.path.getsize(file)
+
 
 # extensions not exhaustive; test purpose
 def get_file_type(file):
@@ -18,9 +20,11 @@ def get_file_type(file):
     elif file_extension in ("txt", ""):
         return "txt"
 
+
 def get_file_dict(file_name):
-    return { "type" : get_file_type(file_name), "name" : file_name, "size" : get_file_size(settings.FILES_PATH + file_name), 
-             "path" : settings.FILES_PATH + file_name }
+    return {"type": get_file_type(file_name), "name": file_name, "size": get_file_size(settings.FILES_PATH + file_name),
+            "path": settings.FILES_PATH + file_name}
+
 
 def get_file_dict_list(file_list):
     file_dict_list = []
@@ -28,30 +32,37 @@ def get_file_dict_list(file_list):
         file_dict_list.append(get_file_dict(file))
     return file_dict_list
 
+
 def create_file(file_name, file_content):
     file = open(settings.FILES_PATH + file_name, "w+")
     file.write(file_content)
+
 
 def upload_file(file):
     if file and is_allowed_extension(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(settings.FILES_PATH, filename))
 
+
 def get_file_content(file_name):
     with open(settings.FILES_PATH + file_name) as file:
         file_content = file.read().replace("\n", "<br>")
     return file_content
 
-def copy_file_to_static(file_name):
+
+def copy_file_to_tmp(file_name):
     shutil.copyfile(settings.FILES_PATH + file_name, settings.CURRENT_PATH + "/static/tmp/" + file_name)
 
-def create_static(path):
+
+def create_tmp():
     if not os.path.exists(settings.CURRENT_PATH + "/static/tmp/"):
         os.makedirs(settings.CURRENT_PATH + "/static/tmp/")
 
-def clear_static():
+
+def clear_tmp():
     shutil.rmtree(settings.CURRENT_PATH + "/static/tmp/")
-    create_static("/static/tmp")
+    create_tmp()
+
 
 def is_allowed_extension(file_name):
     return '.' in file_name and file_name.rsplit('.', 1)[1].lower() in settings.ALLOWED_EXTENSIONS
