c0f7295df077918e8f75b2764cad3c94b378a4b8
jan-klos <janklos@protonmail.com>
2018-05-09 16:07:00 +0200

    confirmation messaged

--- webserver/app.py
+++ webserver/app.py
@@ -3,6 +3,7 @@ import socket  # for IP address testing
 import os
 import util
 import settings
+import messages
 
 app = Flask(__name__)
 
@@ -14,28 +15,30 @@ util.create_tmp()
 
 
 @app.route("/")
-def main(error=""):
+def main(error="", conf=""):
     util.clear_tmp()  # removes temporary image files
     file_list = os.listdir(settings.FILES_PATH)
     file_dict_list = util.get_file_dict_list(file_list)
     template_data = {"files": file_dict_list}
-    return render_template("main.html", error=error, **template_data)
+    return render_template("main.html", error=error, conf=conf, **template_data)
 
 
 @app.route("/", methods=["POST"])
 def add_file():
-    error = ""
+    error, conf = "", ""
     if "create_file_submit" in request.form:
         if request.form["file_name"] == "":
-            error = "You must name the file"
+            error = messages.name_file_err;
         else:
             util.create_file(request.form["file_name"], request.form["file_content"])
+            conf = messages.file_created_conf;
     elif "upload_file_submit" in request.form:
         if not request.files.get("file", None):
-            error = "You must choose a file"
+            error = messages.choose_file_err;
         else:
             util.upload_file(request.files["file"])
-    return main(error=error)
+            conf = messages.file_uploaded_conf;
+    return main(error=error, conf=conf)
 
 
 @app.route("/files/<file_name>", methods=["POST"])
@@ -45,22 +48,22 @@ def my_send_file(file_name):
         try:
             socket.inet_pton(socket.AF_INET, send_address)
         except socket.error:
-            return show_file(file_name, error="Incorrect IP address")
+            return show_file(file_name, error=messages.ip_addr_err)
         util.write_send_info(send_address, file_name)
     elif "download_file_submit" in request.form:
         return send_file(settings.FILES_PATH + file_name, attachment_filename=file_name, as_attachment=True)
-    return redirect("/", code=302)
+    return show_file(file_name, conf=messages.file_sent_conf)
 
 
 @app.route("/files/<file_name>", methods=["GET"])
-def show_file(file_name, error=""):
+def show_file(file_name, error="", conf=""):
     file_dict = util.get_file_dict(file_name)
     if file_dict["type"] in ("img", "pdf"):
         util.copy_file_to_tmp(file_name)
         template_data = {"file": file_dict}
     else:
         template_data = {"file": file_dict, "file_content": util.get_file_content(file_name)}
-    return render_template("file.html", error=error, **template_data)
+    return render_template("file.html", error=error, conf=conf, **template_data)
 
 
 if __name__ == "__main__":
---
+++ webserver/messages.py
@@ -0,0 +1,7 @@
+name_file_err = "You must name a new file"
+choose_file_err = "You must choose a file"
+ip_addr_err = "Incorrect IP address"
+
+file_sent_conf = "File sent"
+file_uploaded_conf = "File uploaded"
+file_created_conf = "File created"
